27b226194fc79d9f811b26cf7f6f5dbd5a7f5b4a
MaximeBattu <m.battu74@gmail.com>
2022-09-09 14:23:23 +0200

    Exercice 1

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 ## Programmation concurrente
 
 Programmer en C et en python
+
+
+## Utils
+
+`gcc -Wall -Wextra arguments.c -o ../bin/arguments`
---
+++ tp1/arguments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    printf("Nom du programme : %s\n", argv[0]);
+    printf("Nombre d’arguments de la ligne de commandes : %d\n", argc - 1);
+    printf("Les arguments sont :\n");
+    for (i = 1; i < argc; i++)
+    {
+        printf("\t%d : %s\n", i, argv[i]);
+    }
+    return 0;
+}
